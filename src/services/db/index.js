636f470ed8ff559de9729129ac4446fe63f6fbf2
Gasprinskiy <85307942+Gasprinskiy@users.noreply.github.com>
2022-07-09 17:27:58 +0500

    Delete index.js

--- src/services/db/index.js
+++
@@ -1,6 +0,0 @@
-import Dexie from 'dexie';
-
-export const db = new Dexie('userDatabase');
-db.version(1).stores({
-  user: '++id, name, fixedSalary, fixedPercenFromSales, isPercentFloats, percentageChangeWhenPlanCompletedOptions, salesPlan', 
-});
