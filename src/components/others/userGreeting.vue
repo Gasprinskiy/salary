9b65ed9c45e6c7948e981bdd9a7314fc22c6679b
Gasprinskiy <85307942+Gasprinskiy@users.noreply.github.com>
2022-07-09 17:45:35 +0500

    Other ui compontents

---
+++ src/components/others/userGreeting.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="user-greeting">
+    <span class="greeting-text">
+        <span v-if="name">
+            <strong>{{name}}</strong>,
+        </span> 
+        {{greeting}}
+    </span>
+  </div>
+</template>
+
+<script>
+
+import {toUpperCaseFirstLetter, greetByTime} from '../../services/helpers'
+
+export default {
+    setup(){
+        return {
+            toUpperCaseFirstLetter, greetByTime
+        }
+    },
+
+    props: {
+        name: {
+            type: String,
+            default: ''
+        }
+    },
+
+    computed: {
+        greeting() {
+            if(this.name === ''){
+                return this.toUpperCaseFirstLetter(`${this.greetByTime()} 👋`)
+            }
+            return `${this.greetByTime()} 👋`
+        }
+    },
+
+}
+</script>
+
+<style lang="scss" scoped>
+    .greeting-text {
+        font-size: 17px;
+        font-weight: 500;
+    }
+</style>
